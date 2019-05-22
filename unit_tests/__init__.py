2766253995b92fc9eef9b29d99251ef9cd1bccf0
cgrindhe <christoffer.hjert.grindheim@cern.ch>
2019-05-22 11:37:01 +0200

    Unit tests now changes CWD to where the __init__ file is placed
    Former-commit-id: e827f23946e7e4e4cb02bab3ac428b8ba8ded80c

--- unit_tests/__init__.py
+++ unit_tests/__init__.py
@@ -1,8 +1,11 @@
 import sys
 import os
 
-search_directory = os.path.dirname(os.path.realpath(__file__))
-search_directory = os.path.split(search_directory)[0]
+this_dir = os.path.dirname(os.path.realpath(__file__))
+if os.getcwd() != this_dir:
+    os.chdir(this_dir)
+
+search_directory = os.path.split(this_dir)[0]
 search_directory += "/tomo"
 if search_directory not in sys.path:
     sys.path.append(search_directory)
