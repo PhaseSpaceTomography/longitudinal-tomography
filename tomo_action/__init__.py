f9d8bd35450446e45470c87c013152c32082d307
cgrindhe <christoffer.hjert.grindheim@cern.ch>
2019-06-12 17:40:24 +0200

    tomo_action __init__ now makes sure cpp lib is compiled
    Former-commit-id: 1a7e727ab5a27060a82640d9dded43e4d96c8c96

--- tomo_action/__init__.py
+++ tomo_action/__init__.py
@@ -1,4 +1,6 @@
 import os
+import sys
+import subprocess
 import logging
 
 logging.basicConfig(level=logging.INFO)
@@ -8,3 +10,12 @@ os.chdir(wanted_working_directory)
 
 if os.getcwd() != wanted_working_directory:
     os.chdir(wanted_working_directory)
+
+# Making suure cpp library is compiled.
+base_dir_path = os.path.realpath(__file__)[:-23]
+cpp_lib_path = base_dir_path + 'tomo/cpp_files/tomolib.so'
+
+if not os.path.isfile(cpp_lib_path):
+    print('compiling...')
+    compilation_script_path = base_dir_path + 'tomo/compile.py'
+    subprocess.call([sys.executable, compilation_script_path])
