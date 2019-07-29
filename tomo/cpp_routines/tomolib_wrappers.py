4e4c7b1149f6de90317d904d1a9dfdcba69c28cf
cgrindhe <christoffer.hjert.grindheim@cern.ch>
2019-07-29 11:56:37 +0200

    Added project and back_project for reconstruction to wrapper file
    Former-commit-id: 35e8a248b1902a49997226fac989029852bd418c

--- tomo/cpp_routines/tomolib_wrappers.py
+++ tomo/cpp_routines/tomolib_wrappers.py
@@ -1,30 +1,73 @@
 import ctypes as ct
+import numpy as np
 import os
 
-tomolib_pth = os.path.dirname(os.path.realpath(__file__)) + '/tomolib.so'
-tomolib = ct.CDLL(tomolib_pth)
+_tomolib_pth = os.path.dirname(os.path.realpath(__file__)) + '/tomolib.so'
+_tomolib = ct.CDLL(_tomolib_pth)
+
+_double_ptr = np.ctypeslib.ndpointer(dtype=np.uintp, ndim=1, flags='C')
+
+_back_project = _tomolib.back_project
+_back_project.argtypes = [np.ctypeslib.ndpointer(ct.c_double),
+                          _double_ptr, np.ctypeslib.ndpointer(ct.c_double)]
+_back_project.restypes = None
+
+_proj = _tomolib.project
+_proj.argtypes = [np.ctypeslib.ndpointer(ct.c_double),
+                  _double_ptr, np.ctypeslib.ndpointer(ct.c_double)]
+_proj.restypes = None
+
+# =============================================================
+# Functions for paricle tracking
+# =============================================================
 
 
 def kick(parameters, denergy, dphi, rfv1, rfv2, nr_part, turn):
-    tomolib.new_kick(_get_pointer(dphi),
-                     _get_pointer(denergy),
-                     ct.c_double(rfv1[turn]),
-                     ct.c_double(rfv2[turn]),
-                     ct.c_double(parameters.phi0[turn]),
-                     ct.c_double(parameters.phi12),
-                     ct.c_double(parameters.h_ratio),
-                     ct.c_int(nr_part),
-                     ct.c_double(parameters.deltaE0[turn]))
+    _tomolib.new_kick(_get_pointer(dphi),
+                      _get_pointer(denergy),
+                      ct.c_double(rfv1[turn]),
+                      ct.c_double(rfv2[turn]),
+                      ct.c_double(parameters.phi0[turn]),
+                      ct.c_double(parameters.phi12),
+                      ct.c_double(parameters.h_ratio),
+                      ct.c_int(nr_part),
+                      ct.c_double(parameters.deltaE0[turn]))
     return denergy
 
 
 def drift(denergy, dphi, dphase, nr_part, turn):
-    tomolib.new_drift(_get_pointer(dphi),
-                      _get_pointer(denergy),
-                      ct.c_double(dphase[turn]),
-                      ct.c_int(nr_part))
+    _tomolib.new_drift(_get_pointer(dphi),
+                       _get_pointer(denergy),
+                       ct.c_double(dphase[turn]),
+                       ct.c_int(nr_part))
     return dphi
+
+# =============================================================
+# Functions for phase space reconstruction
+# =============================================================
+
+
+def back_project(weights, flat_points, flat_profiles, nparts, nprofs):
+    _back_project(weights, _get_2d_pointer(flat_points),
+                  flat_profiles, nparts, nprofs)
+    return weights
+
+
+def project(rec_ps, flat_points, weights, nparts, nprofs, nbins):
+    rec_ps = np.ascontiguousarray(rec_ps.flatten())
+    _proj(rec_ps, _get_2d_pointer(flat_points), weights, nparts, nprofs)
+    rec_ps = rec_ps.reshape((nprofs, nbins))
+    return rec_ps
+
+# =============================================================
+# Utilities
+# =============================================================
 
 
 def _get_pointer(x):
     return x.ctypes.data_as(ct.c_void_p)
+
+
+def _get_2d_pointer(arr2d):
+    return (arr2d.__array_interface__['data'][0]
+            + np.arange(arr2d.shape[0]) * arr2d.strides[0]).astype(np.uintp)
--- tomo/new_tomo_cpp.py
+++ tomo/new_tomo_cpp.py
@@ -1,5 +1,6 @@
 import numpy as np
 import ctypes
+from cpp_routines.tomolib_wrappers import back_project, project
 
 
 class NewTomographyC:
@@ -12,58 +13,34 @@ class NewTomographyC:
                                    self.ts.par.profile_length))
         self.diff = np.zeros(self.ts.par.num_iter + 1)
 
-        # Setting up C++ functions
-        double_ptr = np.ctypeslib.ndpointer(dtype=np.uintp, ndim=1, flags='C')
-        # tomolib_path = './tomo/cpp_routines/tomolib.so'
-        tomolib_path = './cpp_routines/tomolib.so'
-        tomolib = ctypes.CDLL(tomolib_path)
-
-        self._back_proj = tomolib.back_project
-        self._back_proj.argtypes = [np.ctypeslib.ndpointer(ctypes.c_double),
-                                    double_ptr,
-                                    np.ctypeslib.ndpointer(ctypes.c_double)]
-        self._back_proj.restypes = None
-
-        self._proj = tomolib.project
-        self._proj.argtypes = [np.ctypeslib.ndpointer(ctypes.c_double),
-                               double_ptr,
-                               np.ctypeslib.ndpointer(ctypes.c_double)]
-        self._proj.restypes = None
-
     def run_cpp(self):
         nparts = self.tracked_xp.shape[0]
         weights = np.zeros(nparts)
-        flat_points = self.tracked_xp.copy()
-        for i in range(self.ts.par.profile_count):
-            flat_points[:, i] += self.ts.par.profile_length * i
 
-        flat_profs = np.ascontiguousarray(
-                        self.ts.profiles.flatten()).astype(ctypes.c_double)
-        flat_points = np.ascontiguousarray(flat_points).astype(ctypes.c_int)
+        flat_points = self._create_flat_points()
 
-        self._back_proj(weights, self._get_2d_pointer(flat_points),
-                        flat_profs, nparts, self.ts.par.profile_count)
+        flat_profs = np.ascontiguousarray(self.ts.profiles.flatten()
+                                          ).astype(ctypes.c_double)
 
-        # diff_prof = None
+        weights = back_project(weights, flat_points, flat_profs, nparts,
+                               self.ts.par.profile_count)
+
         for i in range(self.ts.par.num_iter):
             print(f'iteration: {str(i + 1)} of {self.ts.par.num_iter}')
 
-            self.recreated = np.ascontiguousarray(self.recreated.flatten())
-            self._proj(self.recreated,
-                       self._get_2d_pointer(flat_points),
-                       weights, nparts, self.ts.par.profile_count)
-
-            self.recreated = self.recreated.reshape(self.ts.profiles.shape)
+            self.recreated = project(self.recreated, flat_points, weights,
+                                     nparts, self.ts.par.profile_count,
+                                     self.ts.par.profile_length)
 
             self.recreated /= np.sum(self.recreated, axis=1)[:, None]
 
             diff_prof = self.ts.profiles - self.recreated
             self.diff[i] = self.discrepancy(diff_prof)
 
-            self._back_proj(weights, self._get_2d_pointer(flat_points),
-                            diff_prof, nparts, self.ts.par.profile_count)
+            weights = back_project(weights, flat_points, diff_prof, nparts,
+                                   self.ts.par.profile_count)
 
-            weights = np.where(weights < 0.0, 0.0, weights)
+            weights = weights.clip(0.0)
 
         # Calculating final discrepancy
         diff_prof = self.ts.profiles - self.recreated
@@ -75,7 +52,8 @@ class NewTomographyC:
         return np.sqrt(np.sum(diff_profiles**2)/(self.ts.par.profile_length
                                                  * self.ts.par.profile_count))
 
-    @ staticmethod
-    def _get_2d_pointer(arr2d):
-        return (arr2d.__array_interface__['data'][0]
-                + np.arange(arr2d.shape[0]) * arr2d.strides[0]).astype(np.uintp)
+    def _create_flat_points(self):
+        flat_points = self.tracked_xp.copy()
+        for i in range(self.ts.par.profile_count):
+            flat_points[:, i] += self.ts.par.profile_length * i
+        return np.ascontiguousarray(flat_points).astype(ctypes.c_int)
