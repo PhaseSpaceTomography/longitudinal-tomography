dddad22393e15ddc1491125feabb0535b6b42031
cgrindhe <christoffer.hjert.grindheim@cern.ch>
2019-08-20 14:58:37 +0200

    Cleaning
    Former-commit-id: e0df26d91da4b47e99bb9607af6113b4b0de55ad

--- tomo/main_testing.py
+++ tomo/main_testing.py
@@ -15,9 +15,7 @@ def main():
     input_path, output_path = get_paths(live=True)
     
     # Collecting time space parameters and data
-    t0 = tm.perf_counter()
     ts = TimeSpace(input_path)
-    time_ts = tm.perf_counter() - t0
 
     ts.save_profiles_text(ts.profiles, output_path, 'py_profiles.dat')
 
@@ -26,39 +24,26 @@ def main():
                               output_path, 'py_vself.dat')
     
     # Creating map outlining for reconstruction
-    t0 = tm.perf_counter()
     mi = MapInfo(ts)
-    time_maps = tm.perf_counter() - t0
 
     mi.write_jmax_tofile(ts, mi, output_path)
     mi.write_plotinfo_tofile(ts, mi, output_path)
 
     # Particle tracking
-    t0 = tm.perf_counter()
     tr = Tracking(ts, mi)
     xp, yp = tr.track()
-    time_tracking = tm.perf_counter() - t0
 
     ta.assert_only_valid_particles(xp, ts.par.profile_length)
 
     # Transposing needed for tomography routine
-    t0 = tm.perf_counter()
+    # NOTE: This takes a notable amount of time
+    #       (~0.5s for C500MidPhaseNoise)
     xp = np.ceil(xp).astype(int).T
     yp = np.ceil(yp).astype(int).T
-    time_transp = tm.perf_counter() - t0
     
-    # Reconstructing phase space
-    t0 = tm.perf_counter()   
+    # Reconstructing phase space  
     tomo = NewTomographyC(ts, xp, yp)
     weight = tomo.run_cpp()
-    time_rec = tm.perf_counter() - t0
-
-    # Writing summary of time usage
-    print(f'Time - time space: {time_ts}s')
-    print(f'Time - map creation: {time_maps}s')
-    print(f'Time - tracking: {time_tracking}s')
-    print(f'Time - Transposing: {time_transp}s')
-    print(f'Time - time recreation: {time_rec}s')
 
     for film in range(ts.par.filmstart - 1, ts.par.filmstop, ts.par.filmstep):
         save_image(xp, yp, weight, ts.par.profile_length, film, output_path)
