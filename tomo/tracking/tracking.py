6432840da40c6e91b4fcc7267eed04ab4ab58a0d
cgrindhe <christoffer.hjert.grindheim@cern.ch>
2019-09-10 10:55:58 +0200

    Cleaning
    Former-commit-id: 6437682d1cdf19493d1c9cb6d200b9ab24d7c7d4

--- tomo/tracking/tracking.py
+++ tomo/tracking/tracking.py
@@ -126,35 +126,6 @@ class Tracking(ParticleTracker):
                 print(f'tracking to profile {profile + 1}')
         return xp, yp
 
-    # def filter_lost_paricles(self, xp, yp):
-    #     tpar = self.timespace.par
-    #     nr_lost_pts = 0
-
-    #     # Find all invalid particle values
-    #     invalid_pts = np.argwhere(np.logical_or(xp >= tpar.profile_length,
-    #                                             xp < 0))
-
-    #     if np.size(invalid_pts) > 0:
-    #         # Find all invalid particles
-    #         invalid_pts = np.unique(invalid_pts.T[1])
-    #         nr_lost_pts = len(invalid_pts)
-
-    #         # Removing invalid particles
-    #         xp = np.delete(xp, invalid_pts, axis=1)
-    #         yp = np.delete(yp, invalid_pts, axis=1)
-
-    #     return xp, yp, nr_lost_pts
-
-
-
-    # def find_nr_of_particles(self):
-    #     jdiff = (self.mapinfo.jmax
-    #              - self.mapinfo.jmin)
-
-    #     pxls = np.sum(jdiff[self.mapinfo.imin
-    #                         :self.mapinfo.imax + 1])
-    #     return int(pxls * self.timespace.par.snpt**2)
-
     def calc_dphi_denergy(self, xp, yp, turn=0):
         tpar = self.timespace.par
         dphi = ((xp + tpar.x_origin)
@@ -164,47 +135,3 @@ class Tracking(ParticleTracker):
                 - tpar.phi0[turn])
         denergy = (yp - tpar.yat0) * self.mapinfo.dEbin
         return dphi, denergy
-
-    # def _populate_bins(self, sqrtNbrPoints):
-    #     xCoords = ((2.0 * np.arange(1, sqrtNbrPoints + 1) - 1)
-    #                / (2.0 * sqrtNbrPoints))
-    #     yCoords = xCoords
-
-    #     xCoords = xCoords.repeat(sqrtNbrPoints, 0).reshape(
-    #         (sqrtNbrPoints, sqrtNbrPoints))
-    #     yCoords = np.repeat([yCoords], sqrtNbrPoints, 0)
-    #     return [xCoords, yCoords]
-
-    # # Wrapper function for creating homogeneously distributed particles
-    # def _initiate_points(self):
-    #     # Initializing points for homogeneous distr. particles
-    #     points = self._populate_bins(self.timespace.par.snpt)
-
-    #     xp = np.zeros(self.find_nr_of_particles())
-    #     yp = np.copy(xp)
-
-    #     # Creating the first profile with equally distributed points
-    #     (xp,
-    #      yp) = self._init_tracked_point(
-    #                     self.timespace.par.snpt, self.mapinfo.imin,
-    #                     self.mapinfo.imax, self.mapinfo.jmin,
-    #                     self.mapinfo.jmax, xp,
-    #                     yp, points[0], points[1])
-
-    #     return xp, yp
-
-    # @staticmethod
-    # @njit
-    # # Creating homogeneously distributed particles
-    # def _init_tracked_point(snpt, imin, imax,
-    #                         jmin, jmax, xp, yp,
-    #                         xpoints, ypoints):
-    #     k = 0
-    #     for iLim in range(imin, imax + 1):
-    #         for jLim in range(jmin[iLim], jmax[iLim]):
-    #             for i in range(snpt):
-    #                 for j in range(snpt):
-    #                     xp[k] = iLim + xpoints[i, j]
-    #                     yp[k] = jLim + ypoints[i, j]
-    #                     k += 1
-    #     return xp, yp
