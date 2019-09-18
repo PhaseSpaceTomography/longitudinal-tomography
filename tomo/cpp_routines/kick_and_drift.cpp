4b93ef3a97e9f5822c3d3c26e6769c5f3ec55700
cgrindhe <christoffer.hjert.grindheim@cern.ch>
2019-09-18 11:17:09 +0200

    Now working GPU kick and drift function, and CPU version
    Former-commit-id: 02b6412278f7bf95203072f5f3178a55f3c2be6e

--- tomo/cpp_routines/kick_and_drift.cpp
+++ tomo/cpp_routines/kick_and_drift.cpp
@@ -1,11 +1,8 @@
 #include <iostream>
-// #include <iomanip>
 #include "sin.h"
+#include <cmath>
 
-using namespace vdt;
-
-// Calculating the energy kick at a turn
-extern "C" void kick(const double * __restrict__ dphi,
+extern "C" void gpu_kick(const double * __restrict__ dphi,
                          double * __restrict__ denergy,
                          const double rfv1,
                          const double rfv2,
@@ -14,26 +11,61 @@ extern "C" void kick(const double * __restrict__ dphi,
                          const double hratio,
                          const int nr_particles,
                          const double acc_kick){
-#pragma omp parallel for
-    for (int i = 0; i < nr_particles; i++)
-        denergy[i] = denergy[i] + rfv1 * fast_sin(dphi[i] + phi0)
-                   + rfv2 * fast_sin(hratio * (dphi[i] + phi0 - phi12));
 
-#pragma omp parallel for
-    for (int i = 0; i < nr_particles; i++)
-        denergy[i] = denergy[i] - acc_kick;
+    #pragma acc parallel loop device_type(nvidia) vector_length(32)
+    for (int i=0; i < nr_particles; i++)    
+        denergy[i] = denergy[i] + rfv1 * std::sin(dphi[i] + phi0)
+                     + rfv2 * std::sin(hratio * (dphi[i] + phi0 - phi12)) - acc_kick;
 }
 
-// Calculating change in phase for each turn
+extern "C" void cpu_kick(const double * __restrict__ dphi,
+                         double * __restrict__ denergy,
+                         const double rfv1,
+                         const double rfv2,
+                         const double phi0,
+                         const double phi12,
+                         const double hratio,
+                         const int nr_particles,
+                         const double acc_kick){
+
+    #pragma omp parallel for
+    for (int i=0; i < nr_particles; i++)    
+        denergy[i] = denergy[i] + rfv1 * vdt::fast_sin(dphi[i] + phi0)
+                     + rfv2 * vdt::fast_sin(hratio * (dphi[i] + phi0 - phi12)) - acc_kick;
+}
+
 extern "C" void drift(double * __restrict__ dphi,
                       const double * __restrict__ denergy,
                       const double dphase,
                       const int nr_particles){
-#pragma omp parallel for
+    #pragma acc parallel loop device_type(nvidia) vector_length(32)
+    #pragma omp parallel for
     for (int i = 0; i < nr_particles; i++)
         dphi[i] -= dphase * denergy[i];
 }
 
+extern "C" void calc_xp_and_yp(double ** __restrict__ xp,           // inn/out
+                               double ** __restrict__ yp,           // inn/out
+                               const double * __restrict__ denergy, // inn
+                               const double * __restrict__ dphi,    // inn
+                               const double phi0,
+                               const double hnum,
+                               const double omega_rev0,
+                               const double dtbin,
+                               const double xorigin,
+                               const double dEbin,
+                               const double yat0,
+                               const int profile,
+                               const int nparts){
+    #pragma acc parallel loop device_type(nvidia) vector_length(32)
+    #pragma omp parallel for
+    for(int i=0; i < nparts; i++){
+        xp[profile][i] = (dphi[i] + phi0) / (hnum * omega_rev0 * dtbin) - xorigin;
+        yp[profile][i] = denergy[i] / dEbin + yat0;
+    }//for
+}
+
+// GPU VERSION
 extern "C" void kick_and_drift(
                          double ** __restrict__ xp,             // inn/out
                          double ** __restrict__ yp,             // inn/out
@@ -56,38 +88,92 @@ extern "C" void kick_and_drift(
                          const int nturns,
                          const int nparts){
     
-    // One should put add a check here that the number of turns corresponds
+    // One should add a check here that the number of turns corresponds
     // to the number of profiles, and then indices in xp and yp arrays.
 
     // std::cout<<std::fixed<<std::setprecision(10);
 
     int profile = 0;
     int turn = 0;
+    int nprofs = (nturns / dturns) + 1;
+    int turn_arr_len = nturns + 1; 
+
+    std::cout << "Tracking..." << std::endl;
+ #pragma acc data pcopyin(dphase[:turn_arr_len], rf1v[:turn_arr_len],\
+                         rf2v[:turn_arr_len],phi0[:turn_arr_len],\
+                         omega_rev0[:turn_arr_len], deltaE0[:turn_arr_len])\
+                         pcopyin(denergy[:nparts], dphi[:nparts])\
+                         pcopyout(xp[:nprofs][:nparts], yp[:nprofs][:nparts])
+    while(turn < nturns){
+        drift(dphi, denergy, dphase[turn], nparts);
+        
+        turn++;
+        
+        gpu_kick(dphi, denergy, rf1v[turn], rf2v[turn], phi0[turn], phi12,
+             hratio, nparts, deltaE0[turn]);
+
+        if(turn % dturns == 0){
+            
+            profile++;
+            
+            calc_xp_and_yp(xp, yp, denergy, dphi, phi0[turn], hnum,
+                           omega_rev0[turn], dtbin, xorigin, dEbin,
+                           yat0, profile, nparts);
+        }// if
+    } //while
+} //func
+
+// CPU VERSION
+extern "C" void kick_and_drift_cpu(
+                         double ** __restrict__ xp,             // inn/out
+                         double ** __restrict__ yp,             // inn/out
+                         double * __restrict__ denergy,         // inn
+                         double * __restrict__ dphi,            // inn
+                         const double * __restrict__ rf1v,      // inn
+                         const double * __restrict__ rf2v,      // inn
+                         const double * __restrict__ phi0,      // inn
+                         const double * __restrict__ deltaE0,   // inn
+                         const double * __restrict__ omega_rev0,// inn
+                         const double * __restrict__ dphase,    // inn
+                         const double phi12,
+                         const double hratio,
+                         const double hnum,
+                         const double dtbin,
+                         const double xorigin,
+                         const double dEbin,
+                         const double yat0,
+                         const int dturns,
+                         const int nturns,
+                         const int nparts){
+    
+    // One should add a check here that the number of turns corresponds
+    // to the number of profiles, and then indices in xp and yp arrays.
+
+    int profile = 0;
+    int turn = 0;
+    int profile_count = nturns / dturns;
 
     std::cout << "Tracking to profile " << profile + 1 << std::endl;
 
     while(turn < nturns){
+
         drift(dphi, denergy, dphase[turn], nparts);
-
+        
         turn++;
-
-        kick(dphi, denergy, rf1v[turn], rf2v[turn], phi0[turn], phi12,
-             hratio, nparts, deltaE0[turn]);
-
+        
+        cpu_kick(dphi, denergy, rf1v[turn], rf2v[turn], phi0[turn], phi12,
+                 hratio, nparts, deltaE0[turn]);   
+        
         if (turn % dturns == 0){
+            
             profile++;
-#pragma omp parallel for
-            for(int i=0; i < nparts; i++){
-                xp[profile][i] = (dphi[i] + phi0[turn])
-                                  / (hnum * omega_rev0[turn] * dtbin)
-                                  - xorigin;
-            } //for xp
-#pragma omp parallel for
-            for(int i=0; i < nparts; i++){
-                yp[profile][i] = denergy[i] / dEbin + yat0;
-            } //for yp
+
+            calc_xp_and_yp(xp, yp, denergy, dphi, phi0[turn], hnum,
+                           omega_rev0[turn], dtbin, xorigin, dEbin,
+                           yat0, profile, nparts);
 
             std::cout << "Tracking to profile " << profile + 1 << std::endl;
+
         } //if
     } //while
-}
+}//func
--- tomo/cpp_routines/test_kick_and_drift.py
+++ tomo/cpp_routines/test_kick_and_drift.py
@@ -5,27 +5,6 @@ import time as tm
 import logging as log
 import numpy.testing as nptest
 
-os.system('clear')
-
-do_compile = True
-if do_compile:
-    print('Compiling!')
-    os.system('rm test.so')
-    os.system('g++ -fopenmp -std=c++11 -shared -O3 -march=native -fPIC -ffast-math -o test.so kick_and_drift.cpp')
-else:
-    print('Using last compilation...')
-
-_tomolib_pth = os.path.dirname(os.path.realpath(__file__)) + '/test.so'
-
-if os.path.exists(_tomolib_pth):
-    log.info(f'Loading test library: {_tomolib_pth}')
-    lib = ct.CDLL(_tomolib_pth)
-else:
-    error_msg = f'\n\nCould not find library at:\n{_tomolib_pth}\n'
-    raise Exeption(error_msg)
-
-_double_ptr = np.ctypeslib.ndpointer(dtype=np.uintp, ndim=1, flags='C') # May be a problem
-
 def _get_pointer(x):
     return x.ctypes.data_as(ct.c_void_p)
 
@@ -34,112 +13,164 @@ def _get_2d_pointer(arr2d):
     return (arr2d.__array_interface__['data'][0]
             + np.arange(arr2d.shape[0]) * arr2d.strides[0]).astype(np.uintp)
 
+def main():
 
-k_and_d = lib.kick_and_drift
-k_and_d.argtypes = [_double_ptr,
-                    _double_ptr,
-                    np.ctypeslib.ndpointer(ct.c_double),
-                    np.ctypeslib.ndpointer(ct.c_double),
-                    np.ctypeslib.ndpointer(ct.c_double),
-                    np.ctypeslib.ndpointer(ct.c_double),
-                    np.ctypeslib.ndpointer(ct.c_double),
-                    np.ctypeslib.ndpointer(ct.c_double),
-                    np.ctypeslib.ndpointer(ct.c_double),
-                    np.ctypeslib.ndpointer(ct.c_double),
-                    ct.c_double,
-                    ct.c_double,
-                    ct.c_double,
-                    ct.c_double,
-                    ct.c_double,
-                    ct.c_double,
-                    ct.c_double,
-                    ct.c_int,
-                    ct.c_int,
-                    ct.c_int]
+    os.system('clear')
+    
+    # Control panel
+    # -----------------
+    do_compile = True
+    gpu = False
+    niter = 1
+    run_tests = True
+    show_all_times = True
+    #------------------
+    
+    cpu_com = 'g++ -fopenmp -march=native -ffast-math'
+    gpu_com = 'pgc++ -acc -Minfo=accel -ta=nvidia'
+    common_com = '-std=c++11 -shared -O3 -fPIC -o test.so kick_and_drift.cpp'
 
-from tomo_v3.unit_tests.C500values import C500
-c500 = C500()
-cv = c500.values
-ca = c500.arrays
+    # minfo can be accell and all
+    
+    if do_compile:
+        print('Compiling!')
+        os.system('rm test.so')
+        if gpu:
+            print('Compiling openACC')
+            os.system(gpu_com + ' ' + common_com)
+        else:
+            print('Compiling openMP')
+            os.system(cpu_com + ' ' + common_com)
+    else:
+        print('Using last compilation...')
+    
+    _tomolib_pth = os.path.dirname(os.path.realpath(__file__)) + '/test.so'
+    
+    if os.path.exists(_tomolib_pth):
+        log.info(f'Loading test library: {_tomolib_pth}')
+        lib = ct.CDLL(_tomolib_pth)
+    else:
+        error_msg = f'\n\nCould not find library at:\n{_tomolib_pth}\n'
+        raise Exeption(error_msg)
+    
+    _double_ptr = np.ctypeslib.ndpointer(dtype=np.uintp, ndim=1, flags='C')
 
-# Getting actual values from c500
-# -----------------------------------------------
-x_origin = cv['xorigin']
-h_num = cv['h_num']
-omega_rev0 = ca['omegarev0']
-dtbin = cv['dtbin']
-phi0 = ca['phi0']
-yat0 = cv['yat0']
-deltaE0 = ca['deltaE0']
-dphase = ca['dphase']
-dturns = cv['dturns']
-phi12 = cv['phi12']
-hratio = cv['hratio']
-hnum = cv['h_num']
-dEbin = cv['debin']
+    k_and_d = lib.kick_and_drift
+    k_and_d.argtypes = [_double_ptr,
+                        _double_ptr,
+                        np.ctypeslib.ndpointer(ct.c_double),
+                        np.ctypeslib.ndpointer(ct.c_double),
+                        np.ctypeslib.ndpointer(ct.c_double),
+                        np.ctypeslib.ndpointer(ct.c_double),
+                        np.ctypeslib.ndpointer(ct.c_double),
+                        np.ctypeslib.ndpointer(ct.c_double),
+                        np.ctypeslib.ndpointer(ct.c_double),
+                        np.ctypeslib.ndpointer(ct.c_double),
+                        ct.c_double,
+                        ct.c_double,
+                        ct.c_double,
+                        ct.c_double,
+                        ct.c_double,
+                        ct.c_double,
+                        ct.c_double,
+                        ct.c_int,
+                        ct.c_int,
+                        ct.c_int]
+    
+    from tomo_v3.unit_tests.C500values import C500
+    c500 = C500()
+    cv = c500.values
+    ca = c500.arrays
+    
+    # Getting actual values from c500
+    # -----------------------------------------------
+    x_origin = cv['xorigin']
+    h_num = cv['h_num']
+    omega_rev0 = ca['omegarev0']
+    dtbin = cv['dtbin']
+    phi0 = ca['phi0']
+    yat0 = cv['yat0']
+    deltaE0 = ca['deltaE0']
+    dphase = ca['dphase']
+    dturns = cv['dturns']
+    phi12 = cv['phi12']
+    hratio = cv['hratio']
+    hnum = cv['h_num']
+    dEbin = cv['debin']
+    
+    # Setting parameters for tracking
+    # ------------------------------------------------
+    # Start parameters:
+    denergy = np.load('/afs/cern.ch/work/c/cgrindhe/tomography/out/denergy.npy')
+    dphi = np.load('/afs/cern.ch/work/c/cgrindhe/tomography/out/dphi.npy')
 
-# Setting parameters for tracking
-# ------------------------------------------------
-# Start parameters:
-denergy = np.load('/afs/cern.ch/work/c/cgrindhe/tomography/out/denergy.npy')
-dphi = np.load('/afs/cern.ch/work/c/cgrindhe/tomography/out/dphi.npy')
+    # other parameters
+    nturns = cv['dturns'] * (cv['profile_count'] - 1)
+    rfv1 = (cv['vrf1'] + cv['vrf1dot'] * ca['time_at_turn']) * cv['q']
+    rfv2 = np.zeros(nturns + 1)
+    
+    nparts = len(denergy)
+    
+    xp = np.zeros((cv['profile_count'], nparts))
+    yp = np.zeros((cv['profile_count'], nparts))
+    xpp = _get_2d_pointer(xp)
+    ypp = _get_2d_pointer(yp)
+    
+    print('Running c++ function...')
+    
+    # =======================================================================
+    # RUNNING TEST
+    # =======================================================================
+    
+    # -----------------------------------------------------------------------
+    # runnin script....
+    # -----------------------------------------------------------------------   
+    
+    run_times = np.zeros(niter, dtype=float)
+   
+    for i in range(niter):
+        t0 = tm.perf_counter()
+        k_and_d(xpp, ypp, denergy, dphi, rfv1, rfv2, phi0, deltaE0,
+                omega_rev0, dphase, phi12, hratio, hnum, dtbin, x_origin,
+                dEbin, yat0, dturns, nturns, nparts)
+        t1 = tm.perf_counter()
+        run_times[i] = t1 - t0
+    
+    total_time = np.sum(run_times)
 
-# other parameters
-nturns = cv['dturns'] * (cv['profile_count'] - 1)
-rfv1 = (cv['vrf1'] + cv['vrf1dot'] * ca['time_at_turn']) * cv['q']
-rfv2 = np.zeros(nturns + 1)
+    print('\nExecution info:\n---------------------')
+    print(f'Compile: {do_compile}')
+    print(f'GPU: {gpu}')
+    print(f'Number of runs: {niter}')
+    print(f'Run tests: {run_tests}')
 
-nparts = len(denergy)
+    if run_tests:
+        print('\nTesting output:\n---------------------')
+        output_dir = '/afs/cern.ch/work/c/cgrindhe/tomography/out'
+    
+        print('testing xp...')
+        old_xp = np.load(output_dir + '/c_xp.npy')
+        nptest.assert_almost_equal(xp, old_xp,
+                                   err_msg='xp was not '\
+                                           'calculated correctly.')
+        print('OK!')
+    
+        print('testing yp...')
+        old_yp = np.load(output_dir + '/c_yp.npy')
+        nptest.assert_almost_equal(yp, old_yp,
+                                   err_msg='yp was not '\
+                                           'calculated correctly.')
+        print('OK!')
+    
+    print('\nTiming:\n---------------------')
+    print(f'time spent: {total_time}')
+    print(f'average time: {total_time / niter}')
 
-xp = np.zeros((cv['profile_count'], nparts))
-yp = np.zeros((cv['profile_count'], nparts))
-xpp = _get_2d_pointer(xp)
-ypp = _get_2d_pointer(yp)
+    if show_all_times:
+        for i, t in enumerate(run_times):
+            print(f'run #{i+1}: {t}')
 
-print('Running c++ function...')
+    # =======================================================================
 
-# =======================================================================
-# RUNNING TEST
-# =======================================================================
-
-# -----------------------------------------------------------------------
-# runnin script....
-# -----------------------------------------------------------------------
-niter = 1
-
-t0 = tm.perf_counter()
-
-for i in range(niter):
-    k_and_d(xpp, ypp, denergy, dphi, rfv1, rfv2, phi0, deltaE0,
-            omega_rev0, dphase, phi12, hratio, hnum, dtbin, x_origin,
-            dEbin, yat0, dturns, nturns, nparts)
-
-t1 = tm.perf_counter()
-dt = t1 - t0
-
-# -----------------------------------------------------------------------
-# Asserting output...
-# -----------------------------------------------------------------------
-print('Output:\n--------------')
-output_dir = '/afs/cern.ch/work/c/cgrindhe/tomography/out'
-
-print('testing xp...')
-old_xp = np.load(output_dir + '/c_xp.npy')
-nptest.assert_almost_equal(xp, old_xp,
-                           err_msg='xp was not '\
-                                   'calculated correctly.')
-print('OK!')
-
-print('testing xp...')
-old_yp = np.load(output_dir + '/c_yp.npy')
-nptest.assert_almost_equal(yp, old_yp,
-                           err_msg='yp was not '\
-                                   'calculated correctly.')
-print('OK!')
-
-print('\nTiming:\n----------------')
-print(f'time spent: {dt}')
-print(f'average time: {dt / niter}')
-
-# =======================================================================
-
+main()
+print()
