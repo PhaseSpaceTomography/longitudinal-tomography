41c1967829c9f0492158bfdd94ae20bdefd26b1b
cgrindhe <christoffer.hjert.grindheim@cern.ch>
2019-10-23 13:57:14 +0200

    Runs for both python and Fortran program.
    Plots now working.
    
    
    Former-commit-id: e9f58a2b3af695febd635e15e3683feb8a58a38d

--- __EXAMPLES/example_scripts/EX_04_Varying_input.py
+++ __EXAMPLES/example_scripts/EX_04_Varying_input.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import os
 import sys
+import subprocess as sub
 import numpy as np
 import time as tm
 from utils.exs_tools import show, make_or_clear_dir
@@ -11,6 +12,8 @@ from parameters import Parameters  # Hack
 BASE_PATH = os.path.dirname(
                 os.path.realpath(__file__)).split('/')[:-1]
 BASE_PATH = '/'.join(BASE_PATH)
+
+FORTRAN_EXE = f'{BASE_PATH}/fortran/tomo_vo.intelmp'
 
 INPUT_FILE_DIR = '/'.join([BASE_PATH] + ['input_files'])
 
@@ -29,23 +32,25 @@ def main():
     # Splitting to parameters and meassured profile data
     raw_parameters = read[:len_input_param]
     raw_data = np.array(read[len_input_param:], dtype=float)
-    del(read)
 
     # Setting output directory
     output_dir_idx = 14
     raw_parameters[output_dir_idx] = f'{output_dir}'
 
-    # Function for varying python input
     rebin_idx = 36
     snpt_idx = 51
+    snpt_py = python_varying_input(raw_parameters, raw_data,
+                                        snpt_idx)
+    rebin_py = python_varying_input(raw_parameters, raw_data,
+                                         rebin_idx)
 
-    snpt_py_vals = python_varying_input(raw_parameters, raw_data,
-                                        snpt_idx, (1, 4))
-    rebin_py_vals = python_varying_input(raw_parameters, raw_data,
-                                         rebin_idx, (6, 8))
+    snpt_ftr = fortran_varying_input(read, output_dir,
+                                          snpt_idx)
+    rebin_ftr = fortran_varying_input(read, output_dir,
+                                           rebin_idx)
 
-    plot(snpt_py_vals, 'snpt')
-    plot(rebin_py_vals, 'rebin')
+    plot(snpt_py, snpt_ftr, 'snpt')
+    plot(rebin_py, rebin_ftr, 'rebin')
 
 
 def python_varying_input(raw_param, raw_data, input_idx, par_range=(1, 10)):
@@ -60,7 +65,7 @@ def python_varying_input(raw_param, raw_data, input_idx, par_range=(1, 10)):
     vals = np.zeros((len(inputs), 3)) - 1
     vals[:,0] = inputs
 
-    rebin = 0
+    failed_runs = []
     for i, in_param in enumerate(inputs):
         raw_param[input_idx] = str(in_param)
         param = Parameters()
@@ -72,26 +77,98 @@ def python_varying_input(raw_param, raw_data, input_idx, par_range=(1, 10)):
             vals[i, 1] = diff[-1]
             vals[i, 2] = t1 - t0 
         except:
-            err_msg = f'Something went wring when running with '\
-                      f'input parameter of the value: {in_param}.'
-            print(err_msg)
-            break
+            failed_runs.append(i)
+
+    vals = np.delete(vals, failed_runs, axis=0)
     return vals
 
-def plot(py_values, tag):
-    parameter_values = py_values[:, 0].astype(int).astype(str)
-    x = py_values[:, 2] # Time
-    y = py_values[:, 1] # Discrepancy
-    y = y * 1000
+def fortran_varying_input(input_file, output_dir,
+                          input_idx, par_range=(1, 10)):
+    input_file = input_file.copy()
 
-    margin = max(y)*0.005 
+    inputs = np.arange(par_range[0], par_range[1])
+    vals = np.zeros((len(inputs), 3)) - 1
+    vals[:,0] = inputs
+
+    # Path as seen from Fortran executable
+    # NB: The output path can have the max length of 60 characters
+    #  for the Fortran program.
+    #  If this number is exceeded, the output will be
+    #  saved to the wrong location.
+    output_idx = 14
+    input_file[output_idx] = '../tmp/\r\n'
+
+    failed_runs = []
+    for i, in_param in enumerate(inputs):
+        
+        make_or_clear_dir(output_dir)
+        
+        input_file[input_idx] = f'{in_param}\r\n'
+        
+        # Save new Fortran input file with updated parameters
+        ftr_input_path = make_new_ftr_input(input_file, output_dir)
+        
+        # Run Fortran exe
+        with open(ftr_input_path) as f_input:
+            t0 = tm.perf_counter()
+            failed = sub.call([FORTRAN_EXE], stdin=f_input)
+            t1 = tm.perf_counter()
+
+        if not failed:
+            diff = find_fortran_discrepancies(output_dir)
+            vals[i, 1] = diff[-1]
+            vals[i, 2] = t1 - t0
+        else:
+            failed_runs.append(i)
+
+    vals = np.delete(vals, failed_runs, axis=0)
+
+    return vals
+
+
+# Save input file for fortran exe
+def make_new_ftr_input(input_file, output_dir):
+    ftr_input_path = f'{output_dir}/input.dat' 
+    with open(ftr_input_path, 'w') as f:
+        for line in input_file:
+            f.write(line)
+    return ftr_input_path
+
+def find_fortran_discrepancies(output_dir):
+    out_files = os.listdir(output_dir)
+
+    discrepancies = None
+    for f in out_files:
+        if f.startswith('d') and f.endswith('.data'):
+            discrepancies = np.genfromtxt(f'{output_dir}/{f}')
+    discrepancies = np.delete(discrepancies, 0, axis=1)
+
+    return discrepancies
+
+
+def plot(py_values, ftr_values, tag):
+    py_vals = py_values[:, 0].astype(int).astype(str)
+    py_x = py_values[:, 2] # Time
+    py_y = py_values[:, 1] # Discrepancy
+    py_y = py_y * 1000
+    margin = max(py_y)*0.005
+
+    ftr_vals = ftr_values[:, 0].astype(int).astype(str)
+    ftr_x = ftr_values[:, 2] # Time
+    ftr_y = ftr_values[:, 1] # Discrepancy
+    ftr_y = ftr_y * 1000 
 
     fig, ax = plt.subplots()
 
-    for i, par_val in enumerate(parameter_values):
-        ax.scatter(x[i], y[i], color='blue')
-        ax.text(x[i]+margin, y[i]+margin, par_val, fontsize=9)
+    ax.scatter(py_x, py_y, color='orange', label='python')
+    for i, par_val in enumerate(py_vals):
+        ax.text(py_x[i]+margin, py_y[i]+margin, par_val, fontsize=9)
+
+    ax.scatter(ftr_x, ftr_y, color='green', label='fortran')
+    for i, par_val in enumerate(ftr_vals):
+        ax.text(ftr_x[i]+margin, ftr_y[i]+margin, par_val, fontsize=9)
     
+    ax.legend()
     ax.set_title(f'Time vs discrepancy - varying the {tag} parameter ')
     ax.set_xlabel('Time [s]')
     ax.set_ylabel('Discrepancy (x1000)')
