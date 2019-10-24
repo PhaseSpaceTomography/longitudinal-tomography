d622a8999f2c84b8c69ec15ccf12ba6149229c0f
cgrindhe <christoffer.hjert.grindheim@cern.ch>
2019-10-24 17:09:50 +0200

    Added routine to manually 'place' and track particles.
    Former-commit-id: e4d2828b59898d9364cc9e6237b438ca4ef57ace

--- __EXAMPLES/example_scripts/EX_05_External_tracking.py
+++ __EXAMPLES/example_scripts/EX_05_External_tracking.py
@@ -1,10 +1,16 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import sys
 import os
 from utils.exs_tools import show, make_or_clear_dir
 sys.path.append('../../tomo')      # Hack
 from main import main as tomo_main # Hack
 from parameters import Parameters  # Hack
+from time_space import TimeSpace
+from map_info import MapInfo
+from tracking.particle_tracker import ParticleTracker
+from cpp_routines.tomolib_wrappers import kick, drift
+
 
 BASE_PATH = os.path.dirname(
                 os.path.realpath(__file__)).split('/')[:-1]
@@ -35,14 +41,96 @@ def main():
     param.parse_from_txt(raw_parameters)
     param.fill()
 
-    rf1vs = (param.vrf1 + param.vrf1dot * param.time_at_turn) * param.q
-    rf2vs = (param.vrf2 + param.vrf2dot * param.time_at_turn) * param.q
+    ts = TimeSpace()
+    ts.create(param, raw_data)
 
-    # Call tracking routine
+    mi = MapInfo(ts)
+    dEbin = mi.find_dEbin()
+
+    # For calculating bucket area
+    mi.find_ijlimits()
+
+    # Particle tracking
+    xp, yp = track_few_parts(ts.par, dEbin)
+    show_few_parts(ts.par, xp, yp, (mi.jmin, mi.jmax))  
 
 
+def track_few_parts(par, dEbin):
 
-    raise NotImplementedError('Example currently under construction.')
+    # Calculating voltage
+    rf1v = (par.vrf1 + par.vrf1dot * par.time_at_turn) * par.q
+    rf2v = (par.vrf2 + par.vrf2dot * par.time_at_turn) * par.q
+
+    # Setting initial coordinates for three particles to be tracked
+    xp_start = np.array([3.2, 25.0, 100.6, 225.0])
+    yp_start = np.array([22.6, 90.8, 66.7, 100.0])
+
+    n_part = len(xp_start)
+    n_turns = par.dturns * (par.profile_count - 1)
+
+    # Creating arrays to be filled by tracking routine
+    xp = np.zeros((par.profile_count, n_part))
+    yp = np.zeros((par.profile_count, n_part))
+
+    # Inserting start values to main array 
+    xp[0] = xp_start
+    yp[0] = yp_start
+
+    # Go from coordinate system to physical values.
+    dphi, denergy = ParticleTracker.coords_to_phase_and_energy(
+                                            par, xp[0], yp[0], dEbin)
+
+    # Track particles
+    xp, yp = kick_and_drift(par, xp, yp, denergy, dphi,
+                            rf1v, rf2v, dEbin, n_turns, n_part)
+
+    return xp, yp
+
+
+# plots the bucket area, start point and trajectories of particles.
+def show_few_parts(par, xp, yp, bucket_area=None):
+
+    fig, ax = plt.subplots()
+    
+    if bucket_area is not None:
+        ax.plot(bucket_area[0], color='black', label='Bucket area')
+        ax.plot(bucket_area[1], color='black')
+
+    ax.plot(xp, yp)
+    ax.scatter(xp[0,:], yp[0,:], marker='.', color='red')
+
+    rang = (-50, par.profile_length + 50)
+    ax.set_xlim(rang)
+    ax.set_ylim(rang)
+    ax.legend()
+
+    plt.show()
+
+
+def kick_and_drift(par, xp, yp, denergy, dphi,
+                   rf1v, rf2v, dEbin, n_turns, n_part):
+    turn = 0
+    profile = 0
+    print('tracking...')
+    while turn < n_turns:
+        # Calculating change in phase for each particle at a turn
+        dphi = drift(denergy, dphi, par.dphase,
+                     n_part, turn)
+        turn += 1
+        # Calculating change in energy for each particle at a turn
+        denergy = kick(par, denergy, dphi, rf1v, rf2v,
+                       n_part, turn)
+        if turn % par.dturns == 0:
+            profile += 1
+            xp[profile] = ((dphi + par.phi0[turn])
+                           / (float(par.h_num)
+                           * par.omega_rev0[turn]
+                           * par.dtbin)
+                           - par.x_origin)
+            yp[profile] = (denergy / float(dEbin)
+                           + par.yat0)
+            print(f'{profile}')
+    return xp, yp
 
     
 if __name__ == '__main__':
