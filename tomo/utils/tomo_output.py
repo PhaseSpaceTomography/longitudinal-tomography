8797a0b6dbec4f4b3621b0dbf35e59da0a8a5957
cgrindhe <christoffer.hjert.grindheim@cern.ch>
2019-11-21 17:28:37 +0100

    Changed Outputhandler from class to module (tomo_output)
    Former-commit-id: b73d29b46e4ffb1cf22077a83163bf021932de4d

---
+++ tomo/utils/tomo_output.py
@@ -0,0 +1,138 @@
+import os
+import sys
+import numpy as np
+import logging as log
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+
+# --------------------------------------------------------------- #
+#                           GENERAL                               #
+# --------------------------------------------------------------- #
+
+# Make sure that output path ends on a dash.
+def adjust_outpath(output_path):
+    if output_path[-1] != '/':
+        output_path += '/'
+    return output_path
+
+# --------------------------------------------------------------- #
+#                           PROFILES                              #
+# --------------------------------------------------------------- #
+
+# Write phase-space image to text-file in tomoscope format.
+# Profiles: waterfall of profiles.
+# rec_prof: index of profile to be reconstructed.
+def save_profile_ftn(profiles, rec_prof, output_dir):
+    out_profile = profiles[rec_prof].flatten()
+    file_path = f'{output_dir}profile{rec_prof + 1:03d}.data'
+    with open(file_path, 'w') as f:
+        for element in out_profile:    
+            f.write(f' {element:0.7E}\n')
+
+
+# Write self volts to text file in tomoscope format.
+# self_fields: calculated self field voltages.
+def save_self_volt_profile_ftn(self_fields, output_dir):
+    out_profile = self_fields.flatten()
+    file_path = f'{output_dir}vself.data'
+    with open(file_path, 'w') as f:
+        for element in out_profile:    
+            f.write(f' {element:0.7E}\n')
+
+# --------------------------------------------------------------- #
+#                         PHASE-SPACE                             #
+# --------------------------------------------------------------- #
+
+# Save phase-space image in a tomoscope format
+# image: recreated phase-space
+# rec prof: index of profile to be reconstructed. 
+def save_phase_space_ftn(image, rec_prof, output_path):
+    log.info(f'Saving image{rec_prof} to {output_path}')
+    image = image.flatten()
+    with open(f'{output_path}image{rec_prof + 1:03d}.data', 'w') as f:
+        for element in image:
+            f.write(f'  {element:0.7E}\n')
+
+# Convert from weighted particles to phase-space image.
+def create_phase_space_image(xp, yp, weight, n_bins, rec_prof):
+    phase_space = np.zeros((n_bins, n_bins))
+    
+    # Creating n_bins * n_bins phase-space image
+    for x, y, w in zip(xp[:, rec_prof], yp[:, rec_prof], weight):
+        phase_space[x, y] += w
+    
+    phase_space = phase_space.clip(0.0)
+    phase_space /= np.sum(phase_space)
+    return phase_space
+
+# --------------------------------------------------------------- #
+#                         DISCREPANCY                             #
+# --------------------------------------------------------------- #
+
+# Write difference to text file in tomoscope format
+def save_difference_ftn(diff, output_path, rec_prof):
+    log.info(f'Saving saving difference to {output_path}')
+    with open(f'{output_path}d{rec_prof + 1:03d}.data', 'w') as f:
+        for i, d in enumerate(diff):
+            f.write(f'         {i:3d}  {d:0.7E}\n')
+
+# --------------------------------------------------------------- #
+#                          TRACKING                               #
+# --------------------------------------------------------------- #
+
+# Write output for particle tracking in Fortran style.
+# The Fortran algorithm is a little different, so
+#  the output concerning lost particles is not valid.
+#  Meanwhile, it is needed for the tomoscope.
+# Profile numbers are added by one in order to compensate for
+#  differences in python and fortran arrays. Fortrans counts from
+#  one, python counts from 0.
+def print_tracking_status_ftn(ref_prof, to_profile):
+    print(f' Tracking from time slice  {ref_prof + 1} to  '\
+          f'{to_profile + 1},   0.000% went outside the image width.')
+
+# --------------------------------------------------------------- #
+#                         END PRODUCT                             #
+# --------------------------------------------------------------- #
+
+# Nice plot of output.
+# Image: recreated phase-space image
+# Diff: array conatining discrepancies for each iteration of reconstruction.
+# Profile: The measured profile to be reconstructed
+def show(image, diff, profile):
+    gs = gridspec.GridSpec(4, 4)
+
+    fig = plt.figure()
+    
+    img = fig.add_subplot(gs[1:, :3])
+    profs1 = fig.add_subplot(gs[0, :3])
+    profs2 = fig.add_subplot(gs[1:4, 3])
+    convg = fig.add_subplot(gs[0, 3])
+
+    cimg = img.imshow(image.T, origin='lower',
+                          interpolation='nearest', cmap='hot')
+
+    profs1.plot(profile, label='measured')
+    profs1.plot(np.sum(image, axis=1),
+                label='reconstructed')
+    profs1.legend()
+
+    profs2.plot(np.sum(image, axis=0),
+                np.arange(image.shape[0]))
+
+    convg.plot(diff, label='discrepancy')
+    convg.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+    convg.legend()
+
+    for ax in (profs1, profs2, convg):
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+    convg.set_xticks(np.arange(len(diff)))
+    convg.set_xticklabels([])
+
+    plt.gcf().set_size_inches(8, 8)
+    plt.tight_layout()
+    plt.show()
