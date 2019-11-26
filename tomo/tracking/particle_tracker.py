9bba3632d6340d507500145750e1429515c42a6b
cgrindhe <christoffer.hjert.grindheim@cern.ch>
2019-11-26 14:31:12 +0100

    Assertion that xat0 is not negative added.
    Former-commit-id: d295ef4837941bb1cf12c08cc1cc03b16b52c3f9

--- tomo/tracking/particle_tracker.py
+++ tomo/tracking/particle_tracker.py
@@ -2,7 +2,8 @@ import numpy as np
 import logging as log
 from machine import Machine
 from particles import Particles
-from utils.assertions import assert_machine
+from utils.assertions import assert_machine, assert_greater_or_equal
+from utils.exceptions import MachineParameterError
 from utils.tomo_output import write_plotinfo_ftn
 
 
@@ -29,8 +30,11 @@ class ParticleTracker:
     def _assert_machine(self, machine):
         needed_parameters = ['vrf1_at_turn', 'vrf2_at_turn', 'q',
                              'nprofiles', 'dphase', 'dturns', 'phi0',
-                             'phi12', 'h_ratio', 'deltaE0']
+                             'phi12', 'h_ratio', 'deltaE0', 'xat0']
         assert_machine(machine, needed_parameters)
+        assert_greater_or_equal(machine.xat0, 'xat0', 0,
+                                MachineParameterError,
+                                'particle tracking needs a valid xat0 value.')
 
     def enable_fortran_output(self, profile_charge):
         self._ftn_flag = True
