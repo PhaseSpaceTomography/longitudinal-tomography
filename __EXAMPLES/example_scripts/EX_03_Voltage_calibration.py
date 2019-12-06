4e8a7aa8b7b467ed4b9d231597761525e51c5caa
cgrindhe <christoffer.hjert.grindheim@cern.ch>
2019-12-06 09:17:46 +0100

    Updated to fit restructured tomography.
    Former-commit-id: ff230c58bcaca1def3dbc68b425a92e6693bc701

--- __EXAMPLES/example_scripts/EX_03_Voltage_calibration.py
+++ __EXAMPLES/example_scripts/EX_03_Voltage_calibration.py
@@ -1,73 +1,56 @@
-import matplotlib.pyplot as plt
-import os
-import sys
 import numpy as np
-from utils.exs_tools import show, make_or_clear_dir
-sys.path.append('../../tomo')      # Hack
-from main import main as tomo_main # Hack
-from parameters import Parameters  # Hack
+import matplotlib.pyplot as plt
 
-BASE_PATH = os.path.dirname(
-                os.path.realpath(__file__)).split('/')[:-1]
-BASE_PATH = '/'.join(BASE_PATH)
+import tomo.particles as parts
+import tomo.tomography.tomography_cpp as tomography
+import tomo.tracking.tracking as tracking
+import tomo.utils.tomo_input as tomoin
 
-INPUT_FILE_DIR = '/'.join([BASE_PATH] + ['input_files'])
 
-def main():
-    input_file = f'{INPUT_FILE_DIR}/flatTopINDIVRotate2.dat'
-    output_dir = f'{BASE_PATH}/tmp'
+input_file_pth = '../input_files/flatTopINDIVRotate2.dat'
+parameter_lines = 98
 
-    make_or_clear_dir(output_dir)
+input_parameters = []
+with open(input_file_pth, 'r') as line:
+    for i in range(parameter_lines):
+        input_parameters.append(line.readline().strip())
 
-    len_input_param = 98
+raw_data = np.genfromtxt(input_file_pth, skip_header=98)
 
-    # Reading input file to memory
-    with open(input_file) as f:
-        read = f.readlines()
+machine, frames = tomoin.txt_input_to_machine(input_parameters)
+waterfall = frames.to_waterfall(raw_data)
 
-    # Splitting to parameters and meassured profile data
-    raw_parameters = read[:len_input_param]
-    raw_data = np.array(read[len_input_param:], dtype=float)
-    del(read)
+ntest_points = 20
+vary_rfv = 1000  # in volts
+rfv_start = machine.vrf1 - vary_rfv
+rfv_end = machine.vrf1 + vary_rfv
+rfv_inputs = np.linspace(rfv_start, rfv_end, ntest_points)
 
-    # Setting output directory
-    output_dir_idx = 14
-    raw_parameters[output_dir_idx] = f'{output_dir}'
+profiles = tomoin.raw_data_to_profiles(
+                waterfall, machine, frames.rebin,
+                frames.sampling_time)
 
-    nsteps = 20
-    rfv_raw_data_idx = 61
-    default_voltage = float(raw_parameters[rfv_raw_data_idx])
-    start_voltage = default_voltage - 500
-    stop_voltage = default_voltage + 500
-    last_differences = []
-    input_volts = []
-    for voltage in np.linspace(start_voltage, stop_voltage, nsteps):
-        raw_parameters[rfv_raw_data_idx] = f'{voltage}'
-        param = Parameters()
-        param.parse_from_txt(raw_parameters)
-        _, diff, _ = tomo_main(param, raw_data)
-        last_differences.append(diff[-1])
-        input_volts.append(voltage)
+diffs = []
+for rfv in rfv_inputs:
+    machine.vrf1 = rfv
+    machine.values_at_turns()
 
-    show(input_volts, last_differences)
+    tracker = tracking.Tracking(machine)
+    xp, yp = tracker.track(machine.filmstart)
 
-def show(input_volts, output_discr):
-    fig, ax = plt.subplots(figsize=(12, 5))
+    xp, yp = parts.physical_to_coords(
+                xp, yp, machine, tracker.particles.xorigin,
+                tracker.particles.dEbin)
+    xp, yp = parts.ready_for_tomography(xp, yp, machine.nbins)
 
-    ax.set_title('Voltage calibration')
-    ax.scatter(input_volts, output_discr)
-    ax.set_xlabel('Voltage')
-    ax.set_ylabel('Discrepancy')
+    tomo = tomography.TomographyCpp(profiles.waterfall, xp)
+    weight = tomo.run_cpp(niter=machine.niter)
 
-    ymin = min(output_discr)
-    ymax = max(output_discr)
-    temp = (ymax - ymin) * 0.05
-    ymin -= temp
-    ymax += temp
+    diffs.append(tomo.diff[-1])
 
-    ax.ticklabel_format(style='sci', axis='y', scilimits=(0,0))
-    ax.set_ylim([ymin, ymax])
-    plt.show()
-
-if __name__ == '__main__':
-    main()
+plt.plot(rfv_inputs, diffs)
+ax = plt.gca()
+ax.set_xlabel('Input voltage')
+ax.set_ylabel('Discrepancy')
+ax.ticklabel_format(axis='y', scilimits=(0,0), style='sci')
+plt.show()
