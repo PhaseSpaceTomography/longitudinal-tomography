2a89f3797eeab7cb5bd7c3b73a227b4dab12e6a0
cgrindhe <christoffer.hjert.grindheim@cern.ch>
2019-12-16 09:46:47 +0100

    Tomography of manually tracked particles implemented. NOT CORRECT OUTPUT.
    Former-commit-id: 61b19a498fb27996810bd203c3bb63b5865b9c8d

--- __EXAMPLES/example_scripts/EX_05_External_tracking.py
+++ __EXAMPLES/example_scripts/EX_05_External_tracking.py
@@ -4,6 +4,8 @@ import os
 from scipy import constants
 import sys
 
+import tomo.tomography.tomography_cpp as tomography
+import tomo.utils.tomo_output as tomoout
 
 def generateBunch(bunch_position, bunch_length,
                   bunch_energy, energy_spread,
@@ -85,8 +87,30 @@ voltage=7945.403672852664           # [V]
 harmonic=1
 eta=0.4344660490259821
 
-# plt.scatter(dphi, denergy)
+dphi_up = np.max(dphi)
+dphi_low = np.min(dphi)
+dphi_bin = (dphi_up - dphi_low) / nbins
+
+# TEMP
+# ------------------------------------------------------------
+# RF wave
+# phases = np.linspace(-np.pi*0.5, np.pi*0.5, 100)
+# rf = voltage * np.sin(phases)
+
+# plt.subplot(211)
+# plt.plot(phases, rf)
+
+# # Particles
+# plt.subplot(212)
+# plt.scatter(dphi[::500], denergy[::500])
+# plt.xlim(-np.pi*0.5, np.pi*0.5)
+
 # plt.show()
+# sys.exit()
+# ------------------------------------------------------------
+# END TEMP
+
+
 all_dphi = np.zeros((nframes, nparts))
 all_denergy = np.zeros((nframes, nparts))
 
@@ -99,11 +123,35 @@ for i in range(nturns):
         all_denergy[int(i / dturns)] = np.copy(denergy)
 print('Tracking complete!')
 
+invalid_pts = np.argwhere(np.logical_or(
+                    all_dphi >= dphi_up, all_dphi < dphi_low))
+    
+nr_lost_pts = 0
+if np.size(invalid_pts) > 0:
+    # Find all invalid particles
+    invalid_pts = np.unique(invalid_pts.T[1])
+    nr_lost_pts = len(invalid_pts)
 
+    # Removing invalid particles
+    all_dphi = np.delete(all_dphi, invalid_pts, axis=1)
+    all_denergy = np.delete(all_denergy, invalid_pts, axis=1)
 
+print(f'Lost {nr_lost_pts} particles.')
 
-# Save particles for each time frame
-# Convert to phase space coordinates
-# tomography(xp, waterfall)
-# tomo.run()
-# show image
+# + some phase in order to get have sync. part.
+#  in the middle of the coordinate system
+all_dphi = (all_dphi / dphi_bin) + 250
+
+denergy_bin = np.max(all_denergy) / nbins
+all_denergy = (all_denergy / denergy_bin) # + energy offset  
+
+all_dphi = np.ascontiguousarray(all_dphi.T.astype(int))
+all_denergy = np.ascontiguousarray(all_denergy.T.astype(int))
+
+tomo = tomography.TomographyCpp(waterfall, all_dphi)
+weight = tomo.run(1)
+
+for rec_idx in range(1):
+    image = tomoout.create_phase_space_image(
+                all_dphi, all_denergy, weight, nbins, rec_idx)
+    tomoout.show(image, tomo.diff, waterfall[rec_idx])
