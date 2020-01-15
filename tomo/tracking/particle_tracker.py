02509c15d2f9a4742035b28d08166dfe761136ef
cgrindhe <christoffer.hjert.grindheim@cern.ch>
2020-01-15 17:54:01 +0100

    Documentation updated.
    Former-commit-id: 246180f1293dd2bdf72afe514f971719694d338d

--- tomo/tracking/particle_tracker.py
+++ tomo/tracking/particle_tracker.py
@@ -1,3 +1,8 @@
+"""Module containing ParticleTracker class
+
+:Author(s): **Christoffer Hjertø Grindheim**
+"""
+
 import numpy as np
 import logging as log
 
@@ -9,8 +14,39 @@ from ..utils import tomo_output as tomoout
 
 
 class ParticleTracker:
+    '''Super class for tracking classes
+    
+    The particle class holds some general utilities por particle tracking.\
+    These includes asserions of parameters and properties for flags.
 
-    # The tracking routine works on a copy of the input coordinates.
+    Parameters
+    ----------
+    machine: Machine
+        Holds all information needed for particle tracking and gneration of\
+        particle distribution.
+
+    Attributes
+    ----------
+    machine: Machine
+        Holds all information needed for particle tracking and gneration of\
+        particle distribution.
+    particles: Particles
+        Creates and/or holds initial distribution of particles.
+    nturns: int
+        Number of machine turns particles should be tracked trough.
+    self_field_flag: property, boolean
+        Flag to indicate that self-fields should be included during tracking.
+    fortran_flag: property, boolean
+        Flag to indicate that the particle tracking should print fortran-style\
+        output strings to stdout during tracking.
+
+    Raises
+    ------
+    MachineParameterError: Exception
+        Input argument is not of type Machine, or Machine object\
+        fields needed for tracking are missing.
+    '''
+    # The tracking routine works on a copy of the input coordinates. <- fix
     def __init__(self, machine):
 
         if not isinstance(machine, mach.Machine):
@@ -22,16 +58,94 @@ class ParticleTracker:
         self.particles = pts.Particles()
 
         self.nturns = machine.dturns * (machine.nprofiles - 1)
-        self._ftn_flag = False
         self._self_field_flag = False
+        self._ftn_flag = False
 
     @property
     def self_field_flag(self):
+        '''self_field_flag defined as @property
+        
+        Flag can be set to true by calling the function: enable_self_fields.
+
+        Returns
+        -------
+        self_field_flag: boolean
+            Flag to indicate that self-fields are to be used during tracking.
+        '''
         return self._self_field_flag
     
     @property
     def fortran_flag(self):
+        '''self_field_flag defined as @property
+
+        Flag can be set to true by calling the function: enable_fortran_output.
+
+        Returns
+        -------
+        fortran_flag: boolean
+            Flag to indicate that Fortran styled output is enabeled.
+        '''
         return self._ftn_flag
+
+    def enable_fortran_output(self, profile_charge):
+        '''Function for enabeling of Fortran-styled output.
+        
+        Call this function in order to produce a Fortran-styled\
+        output during the particle tracking. The output will be\
+        printed to stdout.
+
+        The output will initially be a print of the plot info needed for\
+        the tomoscope application. During the tracking, the output will
+        describe between which profiles the particles are currently\
+        being tracked between. Also, the number of 'lost particles' will be\
+        printed. This is however **not a real measurement**,\
+        but a static string needed due to changes in the tracking algorithm. 
+
+        Parameters
+        ----------
+        profile_charge: float
+            Total charge of profile.
+
+        Raises
+        ------
+        ProfileChargeNotCalculated: Exception
+            Needed field for enabeling Fortran output,\
+            profile charge, is missing from Machine object.
+        '''
+        if profile_charge is None:
+            err_msg = 'profile_charge is needed for fortran-style output'
+            raise expt.ProfileChargeNotCalculated(err_msg)
+        self._ftn_flag = True
+        self._profile_charge = profile_charge
+        log.info('Fortran style output for particle tracking enabled!')
+
+    def enable_self_fields(self, profiles):
+        '''Function for enabeling particle tracking using self-fields.
+
+        Call this function to track the particles using self-fields.
+        Note that the self-field tracking is **much slower** than\
+        tracking without self-fields. 
+
+        Parameters
+        ----------
+        profiles: Profiles
+            Self-field calculations must have been performed in\
+            the Profiles object prior to calling this function.
+
+        Raises
+        ------
+        SelfFieldTrackingError: Exception
+            Needed fields for tracking using self-fields missing\
+            from Machine object.
+        '''
+        needed_fieds = ['phiwrap', 'vself']
+        asrt.assert_fields(profiles, 'profiles', needed_fieds,
+                           expt.SelfFieldTrackingError())
+
+        self._phiwrap = profiles.phiwrap
+        self._vself = profiles.vself
+        self._self_field_flag = True
+        log.info('Tracking using self fields enabled!')
 
     # Checks that the given machine object includes the nesscessary
     # variables to perform the tracking.
@@ -47,23 +161,3 @@ class ParticleTracker:
             machine.synch_part_x, 'synch_part_x',
             0, expt.MachineParameterError,
             'particle tracking needs a valid synch_part_x value.')
-
-    # Only for Fortran output
-    def enable_fortran_output(self, profile_charge):
-        if profile_charge is None:
-            err_msg = 'profile_charge is needed for fortran-style output'
-            raise expt.ProfileChargeNotCalculated(err_msg)
-        self._ftn_flag = True
-        # self._prof.charge only needed for print plotinfo.
-        self._profile_charge = profile_charge
-        log.info('Fortran style output for particle tracking enabled!')
-
-    def enable_self_fields(self, profiles):
-        needed_fieds = ['phiwrap', 'vself']
-        asrt.assert_fields(profiles, 'profiles', needed_fieds,
-                           expt.SelfFieldTrackingError)
-
-        self._phiwrap = profiles.phiwrap
-        self._vself = profiles.vself
-        self._self_field_flag = True
-        log.info('Tracking using self fields enabled!')
