f0a1aa8cfbf1abacdead34774fd1b84ea6ae21ad
cgrindhe <christoffer.hjert.grindheim@cern.ch>
2020-01-20 11:15:51 +0100

    Documentation updated.
    Former-commit-id: 0b62c825d750e3a7c6f38ab77b308669ea3591ca

--- tomo/utils/data_treatment.py
+++ tomo/utils/data_treatment.py
@@ -1,3 +1,8 @@
+'''Module containing functions for threatement of data.
+
+:Author(s): **Christoffer Hjertø Grindheim**
+'''
+
 import numpy as np
 from scipy import optimize
 
@@ -5,10 +10,35 @@ from . import assertions as asrt
 from . import exceptions as expt
 from . import physics
 
-# Original function for subtracting baseline of raw data input profiles.
-# Finds the baseline from the first 5% (by default)
-#  of the beam reference profile.
+
 def calc_baseline_ftn(waterfall, ref_prof, percent=0.05):
+    '''Function for finding baseline of raw data.
+    
+    The function is based on the original Fortran program,
+    and uses a percentage of a reference profile in order to
+    find the baseline of the measurements.
+    
+    Parameters
+    ----------
+    waterfall: ndarray
+        Raw-data shaped as waterfall (nprofiles, nbins).
+    ref_prof: int
+        Index of reference profile.
+    percent: float
+        (optional) A number between 0 and 1 describing the percentage of the
+        reference profile used to find the baseline.
+
+    Returns
+    -------
+    baseline: float
+        Baseline of reference profile.
+
+    Raises
+    ------
+    InputError: Exception
+        Raised if percentage is not given as a float between 0 and 1.
+
+    '''
     asrt.assert_inrange(percent, 'percent', 0.0, 1.0, expt.InputError,
                         'The chosen percent of raw_data '
                         'to create baseline from is not valid')
@@ -20,6 +50,31 @@ def calc_baseline_ftn(waterfall, ref_prof, percent=0.05):
 
 
 def rebin(waterfall, rbn, machine=None, dtbin=None):
+    '''Rebin waterfall from shape (nprofiles, xbins) to (nprofiles, ybins).
+    Based on the rebinning function from the original Fortran program.  
+    
+    Parameters
+    ----------
+    waterfall: ndarray
+        Raw-data shaped as waterfall (nprofiles, nbins).
+    rbn: int
+        Rebinning factor. New number of bins will be nbins/rbn.
+    machine: Machine
+        Object containing machine parameters. If provided, the field
+        holding the number of bins in the profile measurement
+        will be updated.
+    dtbin: float
+        Size of profile bins [s]. If provided, the function
+        will return the new size of the bins after rebinning.
+
+    Returns
+    -------
+    rebinned: ndarray
+        Rebinned waterfall
+    dtbin: float
+        (optional) If a dtbin has been provided in the arguments, the
+        new size of the profile bins will be returned.
+    '''
     data = np.copy(waterfall)
 
     # Check that there is enough data to for the given rebin factor.
@@ -90,16 +145,45 @@ def _rebin_last(data, rbn):
 # Original function for finding synch_part_x
 # Finds synch_part_x based on a linear fit on a refence profile.  
 def fit_synch_part_x(profiles):
+    '''Linear fit to estimate the phase coordinate of the synchronous
+    particle. The estimation is done by using the beam reference profile
+    set in the `Machine` object.
+
+    Parameters
+    ----------
+    profiles: Profiles 
+        Profiles object containing waterfall and information about the
+        measurement.
+
+    Returns
+    -------
+    fitted_synch_part_x
+        X coordinate in the phase space coordinate system of the synchronous
+        particle.
+    tfoot_low
+        Estimation of the lower bunch limit.
+        Needed for 'print plot info' function in order to have
+        the original output format.
+    tfoot_up
+        Estimation of the upper bunch limit.
+        Needed for 'print plot info' function in order to have
+        the original output format.
+    
+    '''
     ref_idx = profiles.machine.beam_ref_frame
     ref_prof = profiles.waterfall[ref_idx] 
     ref_turn = ref_idx * profiles.machine.dturns
 
+    # Find the upper and lower tangent foor of the bunch.
+    # Needed in order to calculate the duration [rad] of the bunch.
     tfoot_up, tfoot_low = _calc_tangentfeet(ref_prof)
-    bunch_duration = (tfoot_up - tfoot_low) * profiles.machine.dtbin
     
+    # Calculate the duration of the bunch [rad].
+    bunch_duration = (tfoot_up - tfoot_low) * profiles.machine.dtbin
     bunch_phaselength = (profiles.machine.h_num * bunch_duration
                          * profiles.machine.omega_rev0[ref_turn])
 
+    # Estimate the synchronous phase.
     x0 = profiles.machine.phi0[ref_turn] - bunch_phaselength / 2.0
     phil = optimize.newton(
             func=physics.phase_low, x0=x0,
@@ -107,6 +191,8 @@ def fit_synch_part_x(profiles):
             tol=0.0001, maxiter=100,
             args=(profiles.machine, bunch_phaselength, ref_turn))
     
+    # Calculates the x coordinate of the synchronous particle given in
+    # the phase space coordinate system.
     fitted_synch_part_x = (tfoot_low + (profiles.machine.phi0[ref_turn] - phil)
                            / (profiles.machine.h_num
                            * profiles.machine.omega_rev0[ref_turn]
@@ -115,8 +201,8 @@ def fit_synch_part_x(profiles):
     return (fitted_synch_part_x, tfoot_low, tfoot_up)
 
 
-# Find foot tangents of profile. Needed to estimate bunch duration
-# when performing a fit to find synch_part_x
+# Finds foot tangents of profile. Needed to estimate bunch duration
+# when performing a fit to find synch_part_x.
 def _calc_tangentfeet(ref_prof):       
     nbins = len(ref_prof)
     index_array = np.arange(nbins) + 0.5
@@ -135,7 +221,7 @@ def _calc_tangentfeet(ref_prof):
     return tanfoot_up, tanfoot_low
 
 
-# return index of last bins to the left and right of max valued bin,
+# Returns index of last bins to the left and right of max valued bin,
 # with value over the threshold.
 def _calc_tangentbins(ref_profile, nbins, threshold_coeff=0.15):
     threshold = threshold_coeff * np.max(ref_profile)
