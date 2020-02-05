6e223d4305a99ed9f4ea63edc5e29c17cd43d1e1
cgrindhe <christoffer.hjert.grindheim@cern.ch>
2020-02-05 13:43:52 +0100

    Tests added
    Former-commit-id: 121f4c3343596e5991608cc8e9e9fbab97086bb0

---
+++ unit_tests/tomography/test_tomography_cpp.py
@@ -0,0 +1,264 @@
+'''Unit-tests for the TomographyCpp class.
+
+Run as python test_tomography_cpp.py in console or via coverage
+'''
+
+import numpy as np
+import numpy.testing as nptest
+import os
+import unittest
+
+import tomo.tomography.tomography_cpp as tmo
+import tomo.utils.exceptions as expt
+
+'''TO TEST
+
+run_hybrid
+ - self.xp is none fails                    [OK]
+ - correct output                           []
+ - waterfall reduced to zero                [OK]
+
+run
+ - self.xp is none fails                    [OK]
+ - correct output                           [OK]
+ - reduced to zero?                         [HOW?]
+
+set xp
+ - not iterable fails                       [OK]
+ - number of dimentions is not two fails    [OK]
+ - wrong number of profiles fails           [OK]
+ - particle outside of image fails          [OK]
+ - Particles can be set to None             [OK]
+ - input_waterfall is <= 0                  [OK]
+
+'''
+
+class TestTomographyCpp(unittest.TestCase):
+
+    def test_set_xp_not_iterable_fails(self):
+        waterfall = np.ones((10, 10))
+        tomo = tmo.TomographyCpp(waterfall)
+        
+        with self.assertRaises(expt.CoordinateImportError,
+                               msg='Providing x coordinates as non iterable '
+                                   'should raise an Exception'):
+            tomo.xp = 1
+
+    def test_set_xp_one_dim_fails(self):
+        waterfall = np.ones((10, 10))
+        tomo = tmo.TomographyCpp(waterfall)
+        
+        with self.assertRaises(expt.CoordinateImportError,
+                               msg='Providing x coordinates array having '
+                                   'another number of dimnetions than two '
+                                   'should raise an Exception'):
+            tomo.xp = [1, 2, 4] 
+
+    def test_set_xp_wrong_nprofs_fails(self):
+        nprofs = 5
+        nbins = 10
+        waterfall = np.ones((nprofs, nbins))
+        tomo = tmo.TomographyCpp(waterfall)
+        
+        nparts = 20
+        pts_nprofs = 2
+        with self.assertRaises(expt.CoordinateImportError,
+                               msg='Providing x coordinates array which has '
+                                   'been tracked trough another number of '
+                                   'profiles than found in the waterfall '
+                                   'should raise an Exception'):
+            tomo.xp = np.ones((nparts, pts_nprofs))
+
+    def test_set_xp_outside_of_img_upper_fails(self):
+        nprofs = 5
+        img_widt = 10
+        waterfall = np.ones((nprofs, img_widt))
+        tomo = tmo.TomographyCpp(waterfall)
+        
+        nparts = 20
+        xp = np.ones((nparts, nprofs))
+        xp[1, 2] = img_widt
+        with self.assertRaises(expt.XPOutOfImageWidthError,
+                               msg='Providing x coordinates array outside '
+                                   'of image widthshould raise an Exception'):
+            tomo.xp = xp
+
+    def test_set_xp_outside_of_img_lower_fails(self):
+        nprofs = 5
+        img_widt = 10
+        waterfall = np.ones((nprofs, img_widt))
+        tomo = tmo.TomographyCpp(waterfall)
+        
+        nparts = 20
+        xp = np.ones((nparts, nprofs))
+        xp[1, 2] = -1
+        with self.assertRaises(expt.XPOutOfImageWidthError,
+                               msg='Providing x coordinates array outside '
+                                   'of image widthshould raise an Exception'):
+            tomo.xp = xp
+        
+    def test_xp_can_be_set_to_none(self):
+        nprofs = 5
+        img_widt = 10
+        nparts = 20
+        waterfall = np.ones((nprofs, img_widt))
+        xp = np.ones((nparts, nprofs))
+        tomo = tmo.TomographyCpp(waterfall, xp)
+        tomo.xp = None
+
+        self.assertEqual(
+            tomo.xp, None, msg='xp should be able to be set to None')
+
+    def test_waterfall_reduced_to_zeros_fail(self):
+        nprofs = 5
+        img_widt = 10
+        waterfall = -np.ones((nprofs, img_widt))
+
+        with self.assertRaises(expt.WaterfallReducedToZero,
+                               msg='Providing an all negative or '
+                                   'zero waterfall should raise an Exception'):
+            tomo = tmo.TomographyCpp(waterfall)
+
+    def test_run_xp_is_none_fails(self):
+        nprofs = 5
+        img_widt = 10
+        waterfall = np.ones((nprofs, img_widt))
+        tomo = tmo.TomographyCpp(waterfall)
+
+        with self.assertRaises(expt.CoordinateError,
+                               msg='Calling the run function, with '
+                                   'tomo.xp=None should raise an Exception'):
+            tomo.run()
+
+    def test_run_correct_weights(self):
+        waterfall = self._load_waterfall()
+
+        nprofs = 10
+        nparts = 50
+        waterfall = waterfall[:nprofs]
+        waterfall = waterfall[:, 70:170]
+        xp = np.meshgrid(np.arange(0, nparts), np.arange(nprofs))[0]
+
+        tomo = tmo.TomographyCpp(waterfall)
+        tomo.xp = xp.T
+        weights = tomo.run(1)
+
+        cweights = np.array([0.00861438, 0.00880599, 0.00893367, 0.00909539,
+                             0.00918903, 0.00922729, 0.00928258, 0.00928246,
+                             0.00927822, 0.00929515, 0.00929504, 0.00921845,
+                             0.0092014,  0.00918014, 0.00915893, 0.00914618,
+                             0.00919728, 0.00912925, 0.00918038, 0.00924009,
+                             0.00920603, 0.00930828, 0.00940621, 0.00953821,
+                             0.00959356, 0.00969566, 0.00981058, 0.00988708,
+                             0.00998924, 0.01007859, 0.01014255, 0.01023618,
+                             0.01021914, 0.01028299, 0.01026588, 0.01021909,
+                             0.01016806, 0.01015526, 0.01011276, 0.01008299,
+                             0.00999782, 0.00992973, 0.00991267, 0.00985308,
+                             0.00977646, 0.0097126,  0.00971697, 0.00967434,
+                             0.00961899, 0.00961908])
+
+        nptest.assert_almost_equal(
+            weights, cweights, err_msg='Weights were calculated incorrectly')
+
+    def test_run_correct_diff(self):
+        waterfall = self._load_waterfall()
+
+        nprofs = 10
+        nparts = 50
+        waterfall = waterfall[:nprofs]
+        waterfall = waterfall[:, 70:170]
+        xp = np.meshgrid(np.arange(0, nparts), np.arange(nprofs))[0]
+
+        tomo = tmo.TomographyCpp(waterfall)
+        tomo.xp = xp.T
+        weights = tomo.run(1)
+
+        correct = 0.009561478717303546
+        self.assertAlmostEqual(tomo.diff[0], correct,
+                               msg='Discrepancy calculated incorrectly')
+
+
+
+    def test_run_hybrid_reduced_to_zeros_fails(self):
+        waterfall = self._load_waterfall()
+
+        nprofs = waterfall.shape[0]
+        nparts = 50
+        xp = np.meshgrid(np.arange(0, nparts), np.arange(nprofs))[0]
+
+        tomo = tmo.TomographyCpp(waterfall, xp.T)
+
+
+        with self.assertRaises(expt.WaterfallReducedToZero,
+                               msg='Waterfall getting reduced to zero '
+                                   'should raise an Exception'):
+            tomo.run_hybrid()
+
+
+    def test_run_hybrid_xp_is_none_fails(self):
+        nprofs = 5
+        img_widt = 10
+        waterfall = np.ones((nprofs, img_widt))
+        tomo = tmo.TomographyCpp(waterfall)
+
+        with self.assertRaises(expt.CoordinateError,
+                               msg='Calling the run_hybrid function, with '
+                                   'tomo.xp=None should raise an Exception'):
+            tomo.run_hybrid()
+                
+    def test_run_hybrid_correct(self):
+        waterfall = self._load_waterfall()
+
+        nprofs = 10
+        nparts = 50
+        waterfall = waterfall[:nprofs]
+        waterfall = waterfall[:, 70:170]
+        xp = np.meshgrid(np.arange(0, nparts), np.arange(nprofs))[0]
+
+        tomo = tmo.TomographyCpp(waterfall)
+        tomo.xp = xp.T
+        weights = tomo.run_hybrid(1)
+
+        cweights = np.array([0.00861438, 0.00880599, 0.00893367, 0.00909539,
+                             0.00918903, 0.00922729, 0.00928258, 0.00928246,
+                             0.00927822, 0.00929515, 0.00929504, 0.00921845,
+                             0.0092014,  0.00918014, 0.00915893, 0.00914618,
+                             0.00919728, 0.00912925, 0.00918038, 0.00924009,
+                             0.00920603, 0.00930828, 0.00940621, 0.00953821,
+                             0.00959356, 0.00969566, 0.00981058, 0.00988708,
+                             0.00998924, 0.01007859, 0.01014255, 0.01023618,
+                             0.01021914, 0.01028299, 0.01026588, 0.01021909,
+                             0.01016806, 0.01015526, 0.01011276, 0.01008299,
+                             0.00999782, 0.00992973, 0.00991267, 0.00985308,
+                             0.00977646, 0.0097126,  0.00971697, 0.00967434,
+                             0.00961899, 0.00961908])
+
+        nptest.assert_almost_equal(
+            weights, cweights, err_msg='Weights were calculated incorrectly')
+
+    def test_run_hybrid_correct_diff(self):
+        waterfall = self._load_waterfall()
+
+        nprofs = 10
+        nparts = 50
+        waterfall = waterfall[:nprofs]
+        waterfall = waterfall[:, 70:170]
+        xp = np.meshgrid(np.arange(0, nparts), np.arange(nprofs))[0]
+
+        tomo = tmo.TomographyCpp(waterfall)
+        tomo.xp = xp.T
+        weights = tomo.run_hybrid(1)
+
+        correct = 0.009561478717303548
+        self.assertAlmostEqual(tomo.diff[0], correct,
+                               msg='Discrepancy calculated incorrectly')
+
+    def _load_waterfall(self):
+        base_dir = os.path.split(os.path.realpath(__file__))[0]
+        base_dir = os.path.split(base_dir)[0]
+        data_path = os.path.join(base_dir, 'resources')
+    
+        waterfall = np.load(os.path.join(
+                        data_path, 'waterfall_INDIVShavingC325.npy'))
+        return waterfall
+
