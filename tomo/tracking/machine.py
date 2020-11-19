a32554b6a7e76312c183243d9327279e3245337b
Anton Lu <anton96lu@gmail.com>
2020-11-19 17:07:02 +0100

    revert Machine change due to circular import
    Former-commit-id: 3dc2c2a970cf11f586180bdb0cb29d87ffdb2a02

--- tomo/tracking/machine.py
+++ tomo/tracking/machine.py
@@ -416,7 +416,7 @@ class Machine:
         # Calculate RF-voltages at each turn
         self.vrf1_at_turn, self.vrf2_at_turn = self._rfv_at_turns()
 
-    def load_fitted_synch_part_x(self,
+    def load_fitted_synch_part_x_ftn(self,
                                  fit_info: Tuple[float, float, float]):
         """Function for setting the synch_part_x if a fit has been performed.
         Saves parameters retrieved from the fitting routine
