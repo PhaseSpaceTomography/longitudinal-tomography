fb931539f78c3b7b4a011456d3d9dee01c420525
Anton Lu <anton96lu@gmail.com>
2021-01-22 18:00:28 +0100

    fix typos
    Former-commit-id: 8c4a0c01b9f53fdf46cea6b38f70690603ac0a7d

--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
 from setuptools import setup
 from os import path
+import platform
 from pybind11.setup_helpers import Pybind11Extension
 
 HERE = path.split(path.abspath(__file__))[0]
+
+extra_compile_args = []
+extra_link_args = []
+if platform.system() == 'Windows':
+    extra_compile_args.append('-openmp')
+elif platform.system() == 'Linux':
+    extra_compile_args.append('-fopenmp')
+    extra_compile_args.append('-ffast-math')
+    extra_link_args.append('-lgomp')
+
 
 cpp_routines = Pybind11Extension('tomo.cpp_routines.libtomo',
                                  sources=['tomo/cpp_routines/libtomo.cpp',
                                           'tomo/cpp_routines/reconstruct.cpp',
                                           'tomo/cpp_routines/kick_and_drift.cpp',
                                           'tomo/cpp_routines/data_treatment.cpp'],
-                                 extra_compile_args=['-fopenmp',
-                                                     '-ffast-math'],
-                                 extra_link_args=['-lgomp'])
+                                 extra_compile_args=extra_compile_args,
+                                 extra_link_args=extra_link_args)
 
 setup(ext_modules=[cpp_routines])
