268e6ce74ef29e4914edfbda9786dd4d063fd3e0
Anton Lu <anton96lu@gmail.com>
2021-05-07 16:20:51 +0200

    fix test_wheels for new manylinux format
    Former-commit-id: c8144e1793a26a0d9825104f62cc26cd371d090f

--- tools/test-wheels.sh
+++ tools/test-wheels.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -u -x
 
-VERSION=`ls wheelhouse | sed 's/longitudinal_tomography-\(.*\)-cp[0-9]\{2\}-cp.*\.whl/\1/' | head -n 1`
+VERSION=$CI_COMMIT_TAG
 # copy tests outside source to test the installed package and not from the repository
 cp -r unit_tests /tmp/
 
