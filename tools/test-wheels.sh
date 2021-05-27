349f62036cc3fd911f5ab50f073fcbeac0e4f65e
Anton Lu <anton96lu@gmail.com>
2021-05-27 15:35:52 +0200

    fix test-wheels pipeline stage
    Former-commit-id: 2c0215d8943f0d4e5d561d1a77f3eea4dba9ff24

--- tools/test-wheels.sh
+++ tools/test-wheels.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-set -e -u -x
+set -e -x
 
-VERSION=$CI_COMMIT_TAG
 # copy tests outside source to test the installed package and not from the repository
 cp -r unit_tests /tmp/
 
@@ -12,6 +11,11 @@ for PYBIN in /opt/python/*/bin/; do
     if [[ "$PYBIN" == *cp2* || "$PYBIN" == *cp35* ]]; then
       # skip python2 and python3.5
       continue
+    fi
+    if [[ -z $CI_COMMIT_TAG ]]; then
+      VERSION=`$PYBIN/python -c "import longitudinal_tomography as tomo; print(tomo.__version__)"`
+    else
+      VERSION=$CI_COMMIT_TAG
     fi
     "$PYBIN/pip" install longitudinal_tomography==$VERSION -f wheelhouse/
     "$PYBIN/pip" install pytest pyyaml
