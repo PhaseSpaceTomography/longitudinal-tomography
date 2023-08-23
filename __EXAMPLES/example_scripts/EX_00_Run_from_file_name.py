e3e53f61747f1ea9e8a67301e02a212b3c5046fa
Bernardo Abreu Figueiredo <bernardo.abreu.figueiredo@cern.ch>
2023-08-23 11:26:05 +0200

    Examples modified to measure times

--- __EXAMPLES/example_scripts/EX_00_Run_from_file_name.py
+++ __EXAMPLES/example_scripts/EX_00_Run_from_file_name.py
@@ -1,3 +1,5 @@
+# %%
+
 # General imports
 import os
 
@@ -8,11 +10,17 @@ import numpy as np
 import longitudinal_tomography.utils.tomo_run as tomorun
 
 from longitudinal_tomography.utils.execution_mode import Mode
+from pyprof import timing
+import time
+
+timing.mode = 'cupy'
+
+start_time = time.time()
 
 ex_dir = os.path.split(os.path.realpath(os.path.dirname(__file__)))[0]
 in_file_pth = os.path.join(ex_dir, 'input_files', 'flatTopINDIVRotate2.dat')
 
-mode = Mode.JIT
+mode = Mode.CUDA
 
 tRange, ERange, density = tomorun.run(in_file_pth, mode=mode)
 
@@ -21,8 +29,14 @@ tRange, ERange, density = tomorun.run(in_file_pth, mode=mode)
 vmin = np.min(density[density > 0])
 vmax = np.max(density)
 
+end_time = time.time()
+
+timing.report(total_time = (end_time - start_time) * 1e3)
+
 plt.contourf(tRange * 1E9, ERange / 1E6, density.T,
              levels=np.linspace(vmin, vmax, 50), cmap='Oranges')
 plt.xlabel('dt (ns)')
 plt.ylabel('dE (MeV)')
 plt.show()
+
+# %%
--- __EXAMPLES/example_scripts/EX_03_Voltage_calibration.py
+++ __EXAMPLES/example_scripts/EX_03_Voltage_calibration.py
@@ -1,3 +1,12 @@
+from pyprof import timing
+import time
+
+timing.mode = 'timing'
+
+start_time = time.time()
+
+timing.start_timing('import_packages')
+
 import os
 
 import matplotlib.pyplot as plt
@@ -8,8 +17,30 @@ import longitudinal_tomography.tracking.particles as parts
 import longitudinal_tomography.tracking.tracking as tracking
 import longitudinal_tomography.utils.tomo_input as tomoin
 
+from longitudinal_tomography.utils.execution_mode import Mode
+
+mode = Mode.CUPY
+
+timing.stop_timing()
+
+if mode == Mode.CUPY or mode == Mode.CUDA:
+    timing.mode = 'cupy'
+    timing.start_timing('import_cupy')
+    import cupy as cp
+    import longitudinal_tomography.tomography.tomography_cupy as tomography_cupy
+    timing.stop_timing()
+
+
+timing.start_timing('read_input_files')
+
+if os.getenv('DATAFILE') is not None:
+    datafile = os.getenv('DATAFILE')
+else:
+    datafile = "flatTopINDIVRotate2.dat"
+
+
 ex_dir = os.path.split(os.path.realpath(os.path.dirname(__file__)))[0]
-in_file_pth = os.path.join(ex_dir, 'input_files', 'flatTopINDIVRotate2.dat')
+in_file_pth = os.path.join(ex_dir, 'input_files', datafile)
 parameter_lines = 98
 
 input_parameters = []
@@ -18,10 +49,13 @@ with open(in_file_pth, 'r') as line:
         input_parameters.append(line.readline().strip())
 
 raw_data = np.genfromtxt(in_file_pth, skip_header=98)
-
+timing.stop_timing()
+timing.start_timing('create_machine_waterfall')
 machine, frames = tomoin.txt_input_to_machine(input_parameters)
 measured_waterfall = frames.to_waterfall(raw_data)
+timing.stop_timing()
 
+timing.start_timing('create_profiles_tomo')
 ntest_points = 20
 vary_rfv = 1000  # volts
 rfv_start = machine.vrf1 - vary_rfv
@@ -32,27 +66,50 @@ profiles = tomoin.raw_data_to_profiles(
     measured_waterfall, machine,
     frames.rebin, frames.sampling_time)
 
-tomo = tomography.TomographyCpp(profiles.waterfall)
+if mode == Mode.CUPY or mode == Mode.CUDA:
+    tomo = tomography_cupy.TomographyCuPy(cp.asarray(profiles.waterfall))
+else:
+    tomo = tomography.TomographyCpp(profiles.waterfall)
+
+timing.stop_timing()
 
 diffs = []
 for rfv in rfv_inputs:
+    timing.start_timing("values_at_turns")
     print(f'Reconstructing using rf-voltage of {rfv:.3f} volt')
     machine.vrf1 = rfv
     machine.values_at_turns()
+    timing.stop_timing()
 
+    timing.start_timing("tracking::create_tracker")
     tracker = tracking.Tracking(machine)
-    xp, yp = tracker.track(machine.filmstart)
+    timing.stop_timing()
+    xp, yp = tracker.track(machine.filmstart, mode=mode)
 
+    timing.start_timing("physical_to_coords")
     xp, yp = parts.physical_to_coords(
         xp, yp, machine, tracker.particles.xorigin,
-        tracker.particles.dEbin)
-    xp, yp = parts.ready_for_tomography(xp, yp, machine.nbins)
+        tracker.particles.dEbin, mode=mode)
+    timing.stop_timing()
+    timing.start_timing("ready_for_tomo")
+    xp, yp = parts.ready_for_tomography(xp, yp, machine.nbins, mode=mode)
+    timing.stop_timing()
 
+    timing.start_timing("set_tomo_xp")
     tomo.xp = xp
-    weight = tomo.run(niter=machine.niter)
+    timing.stop_timing()
+    weight = tomo.run(niter=machine.niter, mode=mode)
     tomo.xp = None
 
     diffs.append(tomo.diff[-1])
+
+end_time = time.time()
+
+if os.getenv("REPORT_FILENAME") is not None and os.getenv("REPORT_FILENAME") != "":
+    report_filename = os.getenv("REPORT_FILENAME")
+    timing.report(total_time = (end_time - start_time) * 1e3, out_file=report_filename)
+else:
+    timing.report(total_time = (end_time - start_time) * 1e3)
 
 plt.plot(rfv_inputs, diffs)
 ax = plt.gca()
