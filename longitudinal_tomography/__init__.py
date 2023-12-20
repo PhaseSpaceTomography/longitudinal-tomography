9aef8cc9972564ddc88f61acde38d4f6d235505b
Bernardo Abreu Figueiredo <bernardo.abreu.figueiredo@cern.ch>
2023-12-20 12:15:01 +0100

    migrated from setup.cfg to pyproject.toml, introduced dynamic package versioning

--- longitudinal_tomography/__init__.py
+++ longitudinal_tomography/__init__.py
@@ -1,16 +1,1 @@
-major_version = 3
-minor_version = 4
-patch_level = 3
-
-dev_version = -1
-
-__version__ = '{}.{}'.format(
-    major_version,
-    minor_version,
-)
-
-if patch_level != 0:
-    __version__ += f'.{patch_level}'
-
-if dev_version != -1:
-    __version__ += '-dev{}'.format(dev_version)
+from ._version import version as __version__
