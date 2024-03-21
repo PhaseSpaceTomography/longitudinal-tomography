6270daf8070cdc5b0ccd1cb180af1c42c3351ee5
Bernardo Abreu Figueiredo <bernardo.abreu.figueiredo@cern.ch>
2024-03-21 14:53:46 +0100

    put imports to function definitions to make still buildable...

--- longitudinal_tomography/__init__.py
+++ longitudinal_tomography/__init__.py
@@ -1,5 +1,3 @@
-from .utils import tomo_config as conf
-
 major_version = 3
 minor_version = 4
 patch_level = 3
@@ -18,13 +16,17 @@ if dev_version != -1:
     __version__ += '-dev{}'.format(dev_version)
 
 def use_gpu():
+    from .utils import tomo_config as conf
     conf.AppConfig.use_gpu()
 
 def use_cpu():
+    from .utils import tomo_config as conf
     conf.AppConfig.use_cpu()
 
 def set_double_precision():
+    from .utils import tomo_config as conf
     conf.AppConfig.set_double_precision()
 
 def set_single_precision():
+    from .utils import tomo_config as conf
     conf.AppConfig.set_single_precision()
